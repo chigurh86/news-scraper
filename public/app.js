724ef298c7b0d6c64c93f455dfbe7b67c9a52a9e
Antonio Caos Jr <antoniocaosjr.@Antonios-MacBook-Pro.local>
2017-05-19 22:10:39 -0400

    test

--- public/app.js
+++ public/app.js
@@ -1,15 +1,3 @@
-$(document).ready(function(){
-  $(document).on('click', '.savenote', function(){
-    var id = $(this).attr("data-id");
-    console.log($(".theComment").val());
-    $.ajax({
-      method: "POST",
-      url: "/stories/save/"+ id,
-      data: {
-        value: $(".theComment").val()
-      }
-    }).done(function(data){
-      // console.log("data"+data);
-    });
-  });
-});
+// $(document).ready(function(){
+//
+// });
