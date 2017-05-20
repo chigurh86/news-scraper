ad0e7e81587c5613186bc510dffbe79aeb69d6fe
Antonio Caos Jr <antoniocaosjr.@Antonios-MacBook-Pro.local>
2017-05-19 22:20:43 -0400

    heroku still not working

--- public/app.js
+++ public/app.js
@@ -1,3 +1,14 @@
-// $(document).ready(function(){
-//
-// });
+$(document).ready(function(){
+  $(document).on('click', '.savenote', function(){
+    var id = $(this).attr("data-id");
+    $.ajax({
+      method: "POST",
+      url: "/stories/save/"+ id,
+      data: {
+        value: $(".theComment").val()
+      }
+    }).done(function(data){
+      // console.log("data"+data);
+    });
+  });
+});
